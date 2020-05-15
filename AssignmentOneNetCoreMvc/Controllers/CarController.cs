324e2a5b9372b3d2de99c540d0561907ad6b47cc
sohampatelk <sohampatelk@yahoo.com>
2020-05-14 21:56:52 -0600

    added CarController

---
+++ AssignmentOneNetCoreMvc/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentOneNetCoreMvc.Controllers
+{
+    public class CarController : Controller
+    {
+        //Actions
+        public IActionResult Index()
+        {
+            // Get Car items from database
+            // Put Car items into a model
+            // Render view using the model
+        }
+    }
+}
